5e8cda7c8ca46e486d065e9519f97939bbb5be41
KoMakiChan <136659762+KoMakiChan@users.noreply.github.com>
2024-06-10 16:20:28 -0500

    Update app.R
    Real Real newest app.R that RUNs

--- app.R
+++ app.R
@@ -151,9 +151,9 @@ server <- function(input, output, session) {
     }, error = function(e) {
       cat("An error occurred during file copy: ", e$message, "\n")
     })
-  
+    
     updateSiteSelectionUI()
-    })
+  })
   
   observeEvent(input$runAnalysis, {
     
@@ -163,29 +163,6 @@ server <- function(input, output, session) {
     #update outputs and visaluzations
     #think about future labs and also company opportunities after i graduate next year
     
-    #get feedback from breeders on what they think is valuable and what kind of outputs they value
-    #for over-performance / under-performance can use maturity checks as yield checks 
-    #check that the actual maturity (DtM) and simulated maturities (stage DOYs) are accurate
-    #investigate structural equation modeling
-    
-    #for location comparison, standardize axes for acc tt and acc tt instead 
-    #  of using real values so that you can plot all the years and locations
-    
-    #build a machine learning model directly off the seasonal parameters instead of just using the apsim yield output
-    #maturity / flowering validation > yield validation for describing the growing season
-    
-    #which of the seasonal variables are affecting the performance of the varieties
-    #rate stress during the different periods as high med low? and use to describe environments 
-    #get feedback from breeders on what they think is valuable and what kind of outputs they value
-    #for over-performance / under-performance can use maturity checks as yield checks 
-    #check that the actual maturity (DtM) and simulated maturities (stage DOYs) are accurate
-    #investigate structural equation modeling
-    
-    #build a machine learning model directly off the seasonal parameters instead of just using the apsim yield output
-    
-    #which of the seasonal variables are affecting the performance of the varieties
-    
-    # Start, set up trials_df -----
     #get feedback from breeders on what they think is valuable and what kind of outputs they value
     #for over-performance / under-performance can use maturity checks as yield checks 
     #check that the actual maturity (DtM) and simulated maturities (stage DOYs) are accurate
@@ -214,8 +191,10 @@ server <- function(input, output, session) {
     setwd("C:/Users/sam/Documents/GitHub/APSIMX_SeasonalCharacterization/apsimx_output")
     #setwd("C:/Users/cmg3/Box/Gilbert/apsimx_output")
     #setwd("~/Library/CloudStorage/Box-Box/apsimx_output")
+    inputdir <- "C:/Users/sam/Documents/GitHub/APSIMX_SeasonalCharacterization/apsimx_output/output"
     
-    trials_df <- read_csv(paste0(codes_dir,"/apsimx_output/output/input.csv")) %>% distinct() %>% mutate(id_trial = row_number()) %>%
+    crop <- "Soy" #  !!! ask Sam if this can be set via a button 
+    trials_df <- read_csv(paste0(inputdir,"/input.csv")) %>% distinct() %>% mutate(id_trial = row_number()) %>%
       rename(X = Longitude, Y = Latitude)
     locs_df <- select(trials_df, X, Y) %>% distinct() %>% mutate(id_loc = row_number())
     trials_df <- left_join(trials_df, locs_df)
@@ -262,7 +241,6 @@ server <- function(input, output, session) {
     dir.create("met", recursive = TRUE, showWarnings = FALSE)
     
     parLapply(cl, seq_len(nrow(locyear_df)), function(idx) {
-      Sys.setlocale("LC_ALL", "English_United States")
       locyear_tmp <- locyear_df[idx, ]
       try({
         met_tmp <- get_daymet2_apsim_met(lonlat = c(locyear_tmp$X, locyear_tmp$Y), 
@@ -330,13 +308,12 @@ server <- function(input, output, session) {
     
     clusterExport(cl, c("trials_df", "codes_dir", "crop", "edit_apsimx", "edit_apsimx_replace_soil_profile", 
                         "paste0", "dir.create", "file.copy", "tryCatch", "print"))
-    
+    Sys.setlocale("LC_ALL", "English_United States")
     #edit the dates so the simulations runs from a month before sowing to a year afterward (max the end of the met file)
     
     # Parallel APSIM files creation
     apsimxfilecreate <- parLapply(cl, 1:nrow(trials_df), function(trial_n) {
       Sys.setlocale("LC_ALL", "English_United States")
-      
       trial_tmp <- trials_df[trial_n,]
       if(!dir.exists(paste0("apsim/trial_",trial_n))) {dir.create(paste0("apsim/trial_",trial_n))}
       source_dir <- paste0("apsim/trial_",trial_n)
@@ -381,7 +358,6 @@ server <- function(input, output, session) {
     # Process each batch
     for (batch in 1:num_batches) {
       # Determine the rows for the current batch
-      Sys.setlocale("LC_ALL", "English_United States")
       batch_rows <- ((batch - 1) * batch_size + 1):min(nrow(trials_df), batch * batch_size)
       batch_trials <- trials_df[batch_rows, ]
       
@@ -391,13 +367,11 @@ server <- function(input, output, session) {
       # Run APSIM simulations in parallel for the current batch
       # Run APSIM simulations in parallel
       results <- parLapply(cl, trial_list, function(trial) {
-        
         Sys.setlocale("LC_ALL", "English_United States")
         trial_n <- trial$id_trial  # Assuming 'id_trial' is the identifier
         source_dir <- paste0("apsim/trial_", trial_n)
         filename <- paste0(crop, "_", trial_n, ".apsimx")
         output <- data.frame()  # Initialize an empty data frame for the results
-        
         log_file <- paste0(source_dir, "/", crop, "_", trial_n, "_log.txt")
         sink(log_file, append = TRUE)
         # Wrap APSIM simulation and result handling in tryCatch to handle any errors
@@ -408,15 +382,14 @@ server <- function(input, output, session) {
           output <- rbind(output, output_tmp)
           # Save individual trial results
           write_csv(output_tmp, file = paste0(source_dir, "/", crop, "_", trial_n, "_out.csv"))
-          message(sprintf("Successfully written file for trial %d", trial_n))
-          sink()  # Close the connection to the log file
+          cat(sprintf("Successfully written file for trial %d", trial_n))
+          
           return(output)  # Return the output for this trial
         }, error = function(e){
           cat(paste0("Simulation for trial ", trial_n, " failed with error: ", e$message, "\n"))
-          sink()
           return(NULL)  # Return NULL if there was an error
         })
-      })                                                                                                                                                                                                                                                                                                                                    
+      })
       
       # Combine the results from this batch and add to the all_results list
       batch_results <- do.call(rbind, results)
@@ -429,6 +402,102 @@ server <- function(input, output, session) {
     
     # Stop the cluster
     stopCluster(cl)
+    
+    
+    # Summarize Results -----
+    
+    # Merge Outputs
+    outfiles <- list.files("apsim/", pattern = "_out", recursive = T)
+    daily_output <- data.table::rbindlist(lapply(outfiles, function(x){read_csv(paste0("apsim/",x),show_col_types = FALSE)}))
+    daily_output <- select(daily_output, -CheckpointID,-SimulationID,-Zone,-Year) %>% arrange(id_trial)
+    
+    # Add cumulative precipitation and thermal time
+    daily_output <- daily_output %>% group_by(id_trial) %>% mutate(AccPrecip = cumsum(Rain), AccTT = cumsum(ThermalTime)) %>% 
+      relocate(AccPrecip, .after = Rain) %>% relocate(AccTT, .after = ThermalTime) 
+    
+    # Get simulated maturity dates 
+    mats <- group_by(daily_output, id_trial) %>% select(Stage, Date, id_trial) %>%
+      filter(Stage == max(Stage)) %>% filter(Date == min(Date)) %>% mutate(MatDate_Sim = date(Date), .keep = "none")
+    #mats <- group_by(daily_output, id_trial) %>% select(StageName, Date, id_trial) %>%
+    #filter(StageName == "Maturing") %>% filter(Date == min(Date)) %>% mutate(MatDate_Sim = date(Date), .keep = "none")
+    
+    # Trim season to one month after death / harvest
+    sim_trim <- group_by(daily_output, id_trial) %>% select(Stage, Date, id_trial) %>%
+      filter(Stage == max(Stage)) %>% filter(Date == max(Date)) %>% 
+      mutate(sim_end = date(Date) %m+% months(1)) %>% select(id_trial, sim_end)
+    daily_output <- daily_output %>% group_by(id_trial) %>% left_join(sim_trim) %>% filter(Date <= sim_end) %>% select(-sim_end)
+    
+    # Format Outputs into the Characterization
+    yields <- group_by(daily_output, id_trial) %>% summarize(Yield_Sim = max(Yieldkgha),  MaxStage = max(Stage))
+    res <- group_by(daily_output, id_trial) %>% filter(!is.na(Result)) %>% select(id_trial, Result)
+    
+    trials_x <- left_join(trials_df, yields) %>% left_join(mats) %>% left_join(res) 
+    trials_x <- select(trials_x, -sim_end) %>% left_join(sim_trim)
+    trials_x <- rename(trials_x, Latitude = Y, Longitude = X) %>%
+      mutate(DTM_Sim = as.numeric(MatDate_Sim - Planting)) %>%
+      relocate(id_trial, id_loc, Site, Latitude, Longitude, Planting, MatDate_Sim, 
+               DTM_Sim, sim_start, sim_end, Year, Genetics, Mat, Yield_Sim)
+    
+    # Periods
+    max_stage <- daily_output %>% 
+      group_by(id_trial) %>% 
+      summarise(max_stage = max(Stage, na.rm = TRUE)) %>% 
+      summarise(max_stage = max(max_stage)) %>% 
+      pull() %>% 
+      round()
+    
+    # Join and mutate
+    daily_output <- daily_output %>% 
+      left_join(select(trials_x, id_trial, MatDate_Sim, Planting), by = "id_trial") %>% 
+      mutate(Period = case_when(
+        Stage == 1 & (as_date(Date) < Planting) ~ 1,
+        Stage == 1 & (as_date(Date) > MatDate_Sim) ~ max(Stage),
+        .default = floor(Stage)
+      )) %>% 
+      select(-MatDate_Sim) %>% 
+      mutate(Period = factor(Period, ordered = TRUE, levels = as.character(1:max_stage)))
+    # daily_output <- daily_output %>% left_join(select(trials_x, id_trial, MatDate_Sim, Planting)) %>% 
+    #   mutate(Stage = case_match(
+    #     Period,
+    #     "1" ~ "Pre-planting", #germinating
+    #     "2" ~ "VE", #emerging
+    #     "3" ~ "V(n)", #vegetative
+    #     "4" ~ "R1", #early flowering
+    #     "5" ~ "R3", #early pod development
+    #     "6" ~ "R5 early", #early grain filling
+    #     "7" ~ "R5 mid", #mid grain filing
+    #     "8" ~ "R5 late", #late grain filling
+    #     "9" ~ "R6", #maturing
+    #     "10" ~ "R7", #ripening
+    #     "11" ~ "R8 & Post-harvest", #harvestripe + germinating
+    #   )) %>% select(-MatDate_Sim) %>% 
+    #   mutate(Period = factor(Period, ordered = T, levels = as.character(1:11)))
+    
+    charact_x <- daily_output %>% 
+      group_by(Period, id_trial) %>% select(-Yieldkgha, -Stage) %>% 
+      mutate(AccPrecip = cumsum(Rain), AccTT = cumsum(ThermalTime)) %>%
+      summarize(across(where(is.numeric) & !c(DOY,AccPrecip,AccTT), function(x){mean(x,na.omit=T)}), 
+                AccPrecip = max(AccPrecip), AccTT = max(AccTT),
+                Start_Date = min(Date), End_Date = max(Date)) %>% 
+      mutate(Length = as.numeric(End_Date - Start_Date), Start_DOY = yday(Start_Date), End_DOY = yday(End_Date)) %>%
+      select(-Start_Date, -End_Date) %>% 
+      relocate(id_trial, Period, Rain) %>% 
+      relocate(AccPrecip, .after = Rain) %>% relocate(AccTT, .after = ThermalTime) %>%
+      relocate(Start_DOY, Length, End_DOY, .after = last_col()) %>%
+      arrange(id_trial) 
+    
+    daily_charact_x <- daily_output
+    
+    unlink("output",recursive = T) ; dir.create("output")
+    write_csv(trials_x, "output/trials_x.csv")
+    write_csv(charact_x, "output/charact_x.csv")
+    write_csv(daily_charact_x, "output/daily_charact_x.csv")
+    
+    
+    #calculate time duration for running the code:
+    end_time <- Sys.time()
+    duration <- end_time - start_time
+    print(duration)
     
     analysisDone(TRUE)
     updateSiteSelectionUI()
@@ -455,7 +524,7 @@ server <- function(input, output, session) {
     })
   }
   
-
+  
   
   updateSiteSelectionFacetdUI <- function() {
     req(analysisDone())
@@ -519,7 +588,7 @@ server <- function(input, output, session) {
     filter(bigmet, day >= 200, day <= 300)
   })
   
-
+  
   # Reactive expression to filter the meteorological data based on selected sites
   filteredMetData <- reactive({
     req(input$selectedSites)
@@ -595,7 +664,7 @@ server <- function(input, output, session) {
     }
   })
   
-output$facetedComparisonPlot <- renderPlot({
+  output$facetedComparisonPlot <- renderPlot({
     req(input$selectedSites_faceted)
     selected_sites <- input$selectedSites_faceted
     req(length(selected_sites) > 0)
@@ -678,7 +747,6 @@ output$facetedComparisonPlot <- renderPlot({
   
   
   heatmap_data <- reactive({
-    gen <- 1
     req(input$heatmapSelect)  # Ensure a variable is selected
     var <- input$heatmapSelect
     # Logic to prepare the heatmap matrix
@@ -802,7 +870,7 @@ output$facetedComparisonPlot <- renderPlot({
   
   
   
-
+  
   
   output$selectSite <- renderUI({
     req(bigmet())

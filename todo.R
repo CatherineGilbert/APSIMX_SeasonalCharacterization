bc5cfa45975f9d752a277d76d761933d7facad25
Catherine Gilbert <cmg3@illinois.edu>
2024-06-10 15:28:56 -0500

    todo list

---
+++ todo.R
@@ -0,0 +1,13 @@
+# descriptions and info boxes for the tool
+  # descriptions of graphs, descriptions of the info in each output (see Seasonal Characterization Slides that were shared)
+# progress bar for process?
+# view results barplot should have the x axis be qualitative- i.e. a bar for each site
+# option to select a folder where to put the results
+# if you hit the "run analysis" button while an analysis is already running, it won't queue another analysis
+# when showing results, round digits to four places
+# when creating graphs, generate the data to be turned into graphs ahead of time, and switch which one is shown with the button
+  #(don't continually run the same code to generate the graphs from scratch every time they're selected)
+# be able to select genetics in the visualizations
+
+# internally:
+# make N/A sowing dates a viable choice

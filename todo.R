b9bb1332d18e4a7082fc2bdc3f05d65ea5b61d50
KoMakiChan <136659762+KoMakiChan@users.noreply.github.com>
2024-06-14 12:17:00 -0500

    Update todo.R
    updated with 6/14 changes

--- todo.R
+++ todo.R
@@ -1,15 +1,21 @@
-# make sure that everything is up to date with the newest version of the code and tools
-# make sure that "download results" button downloads all three files
+Todo:
 # descriptions and info boxes for the tool
-  # descriptions of graphs, descriptions of the info in each output (see Seasonal Characterization Slides that were shared)
+# descriptions of graphs, descriptions of the info in each output (see Seasonal Characterization Slides that were shared)
 # progress bar for process?
 # view results barplot should have the x axis be qualitative- i.e. a bar for each site
-# option to select a folder where to put the results
-# if you hit the "run analysis" button while an analysis is already running, it won't queue another analysis
-# when showing results, round digits to two places
 # when creating graphs, generate the data to be turned into graphs ahead of time, and switch which one is shown with the button
-  #(don't continually run the same code to generate the graphs from scratch every time they're selected)
+  (don't continually run the same code to generate the graphs from scratch every time they're selected)
 # be able to select genetics in the visualizations
 
 # internally:
 # make N/A sowing dates a viable choice
+
+
+Fixed Bugs:
+
+2024/6/14:
+1. make sure that "download results" button downloads all three files
+2. make sure that everything is up to date with the newest version of the code and tools
+3. option to select a folder where to put the results
+4. if you hit the "run analysis" button while an analysis is already running, it won't queue another analysis
+5. when showing results, round digits to two places

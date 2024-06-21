18b9641236d1db4c19814978e3c77f6ef3c9e308
KoMakiChan <136659762+KoMakiChan@users.noreply.github.com>
2024-06-21 13:15:01 -0500

    Update todo.R
    updated todo with what accomplishmented and what need to be done.

--- todo.R
+++ todo.R
@@ -1,7 +1,5 @@
 Todo:
-# descriptions and info boxes for the tool
 # descriptions of graphs, descriptions of the info in each output (see Seasonal Characterization Slides that were shared)
-# progress bar for process?
 # view results barplot should have the x axis be qualitative- i.e. a bar for each site
 # when creating graphs, generate the data to be turned into graphs ahead of time, and switch which one is shown with the button
   (don't continually run the same code to generate the graphs from scratch every time they're selected)
@@ -11,7 +9,21 @@ Todo:
 # make N/A sowing dates a viable choice
 
 
+Major Issues:
+1. trials_df not found: process with shiny app and parallel programming, not updated environment after each step. 
+2. display of facted comparision and between sites, similar issues with environmental variables. 
+3. small_charact_dt has 141 lines but only 97 apsimx trials. Why? 
+
+
+
+
+
 Fixed Bugs:
+
+2024/6/21:
+# descriptions and info boxes for the tool
+# progress bar for process
+
 
 2024/6/14:
 1. make sure that "download results" button downloads all three files
